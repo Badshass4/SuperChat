9f4edfe62adf58024b4c24da734b6529b447823d
Badshass4 <badsha1695@gmail.com>
2021-10-10 11:58:48 +0530

    manual changes added for google firebase

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -1,26 +1,44 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 interface LoginScreenProps {
   useEffectHook: any;
   text: string;
   setText: Function;
+  googleIcon: any;
 }
 
 const LoginScreen = () => {
   const [text, setText] = useState('Hello from Login Screen');
+  const googleIcon = <Icon name="google" size={30} />;
   return (
-    <LoginScreenProps useEffectHook={useEffect} text={text} setText={setText} />
+    <LoginScreenProps
+      useEffectHook={useEffect}
+      text={text}
+      setText={setText}
+      googleIcon={googleIcon}
+    />
   );
 };
 
 export const LoginScreenProps = (props: LoginScreenProps) => {
-  const {useEffectHook, text, setText} = props;
+  const {useEffectHook, text, setText, googleIcon} = props;
+
+  const onGooglePress = () => {
+    console.log('google');
+  };
 
   return (
     <View style={styles.mainContainer}>
       <Text>{text}</Text>
+      <TouchableOpacity onPress={onGooglePress}>
+        <View style={styles.iconView}>
+          {googleIcon}
+          <Text style={styles.googleText}>oogle</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -34,5 +52,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  iconView: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  googleText: {
+    fontSize: 28,
   },
 });
